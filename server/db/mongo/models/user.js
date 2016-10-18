9186da21cfe3caae417b0b7fb134e17b6259c089
I Putu Yoga Permana <putuyoga@gmail.com>
2016-10-18 14:34:35 +0800

    add `role` field to user schema

--- server/db/mongo/models/user.js
+++ server/db/mongo/models/user.js
@@ -15,6 +15,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String,
+  role: String,
   tokens: Array,
   profile: {
     name: { type: String, default: '' },
